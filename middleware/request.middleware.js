679bea0712b1bb04af90d0df42cada1614ac162d
Jorge Hernandez <jorgehdzg1991@gmail.com>
2018-11-14 11:07:23 -0600

    Changed file identation

--- middleware/request.middleware.js
+++ middleware/request.middleware.js
@@ -2,17 +2,17 @@ const { verifyJWT } = require('../helpers/auth.helper');
 const { UNAUTHORIZED } = require('http-status-codes');
 
 function verifyJWTMiddleware(req, res, next) {
-  let token = req.headers['auth-jwt-token'];
+    let token = req.headers['auth-jwt-token'];
 
-  verifyJWT(token)
-    .then((decodedToken) => {
-      req.user = decodedToken.data;
-      next();
-    })
-    .catch(() => {
-      res.status(UNAUTHORIZED)
-        .json({ message: "Invalid auth token provided." });
-    })
+    verifyJWT(token)
+        .then((decodedToken) => {
+            req.user = decodedToken.data;
+            next();
+        })
+        .catch(() => {
+            res.status(UNAUTHORIZED)
+                .json({ message: "Invalid auth token provided." });
+        })
 }
 
 module.exports = { verifyJWTMiddleware };
